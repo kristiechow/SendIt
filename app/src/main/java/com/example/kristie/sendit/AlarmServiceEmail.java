61a34fd881dd7077eb1a6ca06ac826df7644e98b
Nayib Asis <nayib.asis.elizalde.20@dartmouth.edu>
2017-11-19 14:15:27 -0500

    Nayib's push final app

---
+++ app/src/main/java/com/example/kristie/sendit/AlarmServiceEmail.java
@@ -0,0 +1,38 @@
+package com.example.kristie.sendit;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by nayibasiselizalde on 11/19/17.
+ */
+
+public class AlarmServiceEmail extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent i, int flags, int startId) {
+        // TODO Auto-generated method stub
+        String contact = i.getStringExtra("contact");
+        String subject = i.getStringExtra("subject");
+        String body = i.getStringExtra("body");
+
+        GMailSender gMailSender = new GMailSender("nayib.asis@gmail.com", "lenaBem@n54a");
+        try {
+            gMailSender.sendMail(subject, body, "nayib.asis@gmail.com", contact);
+            Log.d("GMAIL", "WORKS");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return START_STICKY;
+    }
+
+}
